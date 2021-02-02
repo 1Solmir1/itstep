91a15901a9211d59d7a579876b18a6ac5652b12a
1Solmir1 <78374922+1Solmir1@users.noreply.github.com>
2021-02-02 22:32:16 +0200

    Lesson3

---
+++ Lesson3/poem.py
@@ -0,0 +1,2 @@
+input("If you wanna see the poem, please press Enter")
+poem = input("Пустое вы сердечным ты \nОна, обмолвясь, заменила""\nИ все счастливые мечты""\nВ душе влюбленной возбудила")
---
+++ Lesson3/variables.py
@@ -0,0 +1,21 @@
+current_year = 2021
+
+light_speed_in_vacum = 299792.458
+
+quote = "Можливо все, неможливе просто потребує більше часу." "\n Ден Браун"
+
+oceans = ["Pacific", "Atlantic", "Indian", "Southern", "Arctic"]
+
+active = True
+
+naming = ("i", "t", "s", "t", "e", "p")
+
+school = {"students amount" : 5, "students" : ["Neo", "Triniti", "Morpheus", "Mouse", "Tank"], "subjects" : ("Python", "Math", "English", "Kung-Fu")}
+
+print(current_year)
+print(light_speed_in_vacum)
+print(quote)
+print(oceans)
+print(active)
+print(naming)
+print(school)
